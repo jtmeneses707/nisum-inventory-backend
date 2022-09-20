e5e10cae1acc662bcc7aa6b92e66f3e3e99de1e9
jtran81124 <43736400+jtran81124@users.noreply.github.com>
2022-09-20 13:34:05 -0700

    Revert "A922 updated from dev branch"

--- src/main/java/com/ascend/components/repositories/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ascend.components.repositories;
-
-import com.ascend.components.entities.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductsRepository extends JpaRepository<Products, Integer> {
-}
--- src/main/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplication.java
+++ src/main/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplication.java
@@ -1,15 +1,9 @@
 package com.ascend.nisuminventoryapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.*")
-@EnableJpaRepositories(basePackages = "com.*")
-@EntityScan(basePackages = "com.*")
-@EnableAutoConfiguration
+@SpringBootApplication
 public class NisumInventoryApiApplication {
 
   public static void main(String[] args) {

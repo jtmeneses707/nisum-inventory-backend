e64f29e9d40fdf965f70ff9b3e503c4845376f87
Raul Cervantes <cervantesvraul@gmail.com>
2022-09-20 11:42:43 -0700

    Added packages and files to start out

--- src/main/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplication.java
+++ src/main/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplication.java
@@ -1,9 +1,15 @@
 package com.ascend.nisuminventoryapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.*")
+@EnableJpaRepositories(basePackages = "com.*")
+@EntityScan(basePackages = "com.*")
+@EnableAutoConfiguration
 public class NisumInventoryApiApplication {
 
   public static void main(String[] args) {

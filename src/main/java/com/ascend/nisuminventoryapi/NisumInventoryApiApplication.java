46c9d169bda56c74fe39bea7050e311a93645451
jtmeneses707 <jtmenese@gmail.com>
2022-09-20 09:30:11 -0700

    chore: init commit

---
+++ src/main/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ascend.nisuminventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumInventoryApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NisumInventoryApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ascend/nisuminventoryapi/NisumInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.nisuminventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumInventoryApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

746a696d7fa191557ba9ee52f7231df5cf0a1547
Ahmed Damanhoury <adamanhoury7723@sdsu.edu>
2022-09-23 12:10:48 -0700

    changed search testing to test from bodycontent verses pagesource and added page view test

---
+++ src/test/java/com/selenium/PageViewTest.java
@@ -0,0 +1,36 @@
+package com.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageViewTest {
+
+    @Test
+    public void checkChrome() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/test/java/com/selenium/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://127.0.0.1:4200/"); //opens local host / port number
+        String bodyText = driver.findElement(By.className("container")).getText();
+        Thread.sleep(2000);
+        Assert.assertTrue(bodyText.contains("036000291452\n" +
+                "Chicken\n" +
+                "food\n" +
+                "4.2\n" +
+                "10\n" +
+                "20\n" +
+                "5"));
+
+        Assert.assertTrue(bodyText.contains("111111111114\n" +
+                "Waffles\n" +
+                "Food\n" +
+                "3.75\n" +
+                "153345\n" +
+                "12354\n" +
+                "22000"));
+        driver.close();
+
+    }
+}

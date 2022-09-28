98727115878ac0e0fbf9bb0c35067956e72e6f70
ElizabethFeng <111803548+ElizabethFeng@users.noreply.github.com>
2022-09-28 15:39:55 -0700

    ELIZABETH: ADDING API GET METHOD W RESPONSE BODY TEST

--- src/test/java/com/api/A922125Test.java
+++ src/test/java/com/api/A922125Test.java
@@ -1,6 +1,7 @@
 package com.api;
 
 import com.ascend.nisuminventoryapi.NisumInventoryApiApplication;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,8 +22,52 @@ public class A922125Test {
     private MockMvc mockMvc;
 
     @Test
-    public void SearchByEndpointTest() throws Exception {
+    public void SearchByEndpointTestOneSearch() throws Exception {
 
+        //test one inventory value first
+        JSONObject body = new JSONObject();
+
+        body.put(apiConstants.CATEGORY, "snacks");
+
+        this.mockMvc
+                .perform(get("/api/products/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body.toJSONString()))
+                .andDo(print()).andExpect(status().isOk());
     }
 
+    @Test
+    public void SearchByEndpointTestTwoSearch() throws Exception {
+
+        //test one inventory value first
+        JSONObject body = new JSONObject();
+
+        body.put(apiConstants.CATEGORY, "snacks");
+        body.put(apiConstants.BRAND,"peperidge farms");
+
+
+        this.mockMvc
+                .perform(get("/api/products/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body.toJSONString()))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void SearchByEndpointTestMaxSearch() throws Exception {
+
+        //test one inventory value first
+        JSONObject body = new JSONObject();
+
+        body.put(apiConstants.CATEGORY, "snacks");
+        body.put(apiConstants.BRAND,"peperidge farms");
+        body.put(apiConstants.UPC,"134561728334");
+
+
+        this.mockMvc
+                .perform(get("/api/products/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body.toJSONString()))
+                .andDo(print()).andExpect(status().isOk());
+    }
 }
